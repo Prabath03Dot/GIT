a8b364014d7a99149072194f784154e2f37b31d0
Prabath03Dot <48862955+Prabath03Dot@users.noreply.github.com>
2019-11-28 14:04:15 +0530

    Update alto.java

--- alto.java
+++ alto.java
@@ -2,4 +2,11 @@ class Apples{
 	public static void main(String args[]){
 		System.out.println("Hello world:");
 	}
+	
+} 
+class dude{
+	public static void (String args[]){
+		System.out.println("Hello world:");
+	}
+	
 } 
